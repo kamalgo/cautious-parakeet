fbb76b5695d4fa3375bc965b1cda8fb1c8f983ab
kamalgo <kamal.252534@gmail.com>
2025-02-20 15:12:44 +0530

    added redis pass to the code

--- backend/database/redisClient.js
+++ backend/database/redisClient.js
@@ -1,3 +1,26 @@
+// const redis = require("redis");
+// const dotenv = require("dotenv");
+
+// dotenv.config(); // Load environment variables
+
+// // Create Redis Client
+// const redisClient = redis.createClient({
+//     socket: {
+//         host: process.env.REDIS_HOST,
+//         port: process.env.REDIS_PORT
+//     },
+//     password: process.env.REDIS_PASSWORD || null
+// });
+
+// // Handle Redis Connection Events
+// redisClient.on("connect", () => console.log("✅ Redis Connected"));
+// redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
+
+// // Connect Redis
+// redisClient.connect();
+
+// module.exports = redisClient;
+
 const redis = require("redis");
 const dotenv = require("dotenv");
 
@@ -9,7 +32,7 @@ const redisClient = redis.createClient({
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT
     },
-    password: process.env.REDIS_PASSWORD || null
+    password: process.env.REDIS_PASSWORD || null  // Optional if password is not required
 });
 
 // Handle Redis Connection Events
