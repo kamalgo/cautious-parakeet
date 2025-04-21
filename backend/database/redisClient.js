54702c98d9c32a0e801332a33ce50e45929aca52
kamalgo <kamal.252534@gmail.com>
2025-04-21 10:11:04 +0530

    redis

--- backend/database/redisClient.js
+++ backend/database/redisClient.js
@@ -1,24 +1,24 @@
-// const redis = require("redis");
+const redis = require("redis");
 
-// // Create Redis client for local EC2 instance
-// const redisClient = redis.createClient({
-//     socket: {
-//         host: process.env.REDIS_HOST || "127.0.0.1", // Default to local Redis
-//         port: process.env.REDIS_PORT || 6379
-//     }
-// });
+// Create Redis client for local EC2 instance
+const redisClient = redis.createClient({
+    socket: {
+        host: process.env.REDIS_HOST || "127.0.0.1", // Default to local Redis
+        port: process.env.REDIS_PORT || 6379
+    }
+});
 
-// redisClient.on("connect", () => console.log("✅ Redis Connected"));
-// redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
+redisClient.on("connect", () => console.log("✅ Redis Connected"));
+redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
 
-// (async () => {
-//     try {
-//         await redisClient.connect();
-//         console.log("🚀 Redis connection established!");
-//     } catch (err) {
-//         console.error("❌ Redis Connection Failed:", err);
-//     }
-// })();
+(async () => {
+    try {
+        await redisClient.connect();
+        console.log("🚀 Redis connection established!");
+    } catch (err) {
+        console.error("❌ Redis Connection Failed:", err);
+    }
+})();
 
-// module.exports = redisClient;
+module.exports = redisClient;
 
