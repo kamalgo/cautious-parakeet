2e67cab7b7ffcce04101354a75c19c3ed72e76dc
kamalgo <kamal.252534@gmail.com>
2025-04-21 10:34:54 +0530

    removed commas

--- backend/gemini_ocr/extractFieldsFromImageURL.js
+++ backend/gemini_ocr/extractFieldsFromImageURL.js
@@ -32,7 +32,7 @@ async function extractFieldsFromImageURL(imageUrl, docType) {
     case "incomeDoc":
       prompt = `From this income certificate image, extract the information and translate any Marathi text into English. Return the extracted information as JSON with the following fields:
       {
-        "IncomeAmount": "", //convert from Marathi to English
+        "IncomeAmount": "", //convert from Marathi to English and format as a number there should be no commas
         "CertificateNo": "" //convert from Marathi to English
       }`;
       break;
