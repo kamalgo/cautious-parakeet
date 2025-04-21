42230f6f21ed9f69dbc7f2aea869103d58b53e73
kamalgo <kamal.252534@gmail.com>
2025-04-21 10:24:00 +0530

    added promt mar to eng

--- backend/gemini_ocr/extractFieldsFromImageURL.js
+++ backend/gemini_ocr/extractFieldsFromImageURL.js
@@ -30,7 +30,7 @@ async function extractFieldsFromImageURL(imageUrl, docType) {
 
   switch (docType) {
     case "incomeDoc":
-      prompt = `From this income certificate image, extract the information cand convert it in English and return as JSON:
+      prompt = `From this income certificate image, extract the information and translate any Marathi text into English. Return the extracted information as JSON with the following fields:
       {
         "Name": "",
         "IncomeAmount": "",
